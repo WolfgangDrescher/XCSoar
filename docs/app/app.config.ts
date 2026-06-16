041299769fe3b59421d6e559987634ea3b4414a7
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2026-06-16 12:39:32 +0200

    Docs: Improve h2 styles

--- docs/app/app.config.ts
+++ docs/app/app.config.ts
@@ -11,4 +11,14 @@ export default defineAppConfig({
         branch: 'master',
         rootDir: 'docs',
     },
+    ui: {
+        prose: {
+            h2: {
+                slots: {
+                    base: ['text-3xl'],
+                    link: 'text-primary-500',
+                },
+            },
+        },
+    },
 });
