f5c6a5836588f3863ce742db903ebfacac616b7a
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2026-09-16 20:06:55 +0000

    Dialogs/Settings/Panels/VarioConfigPanel: Show the settings as a grouped list

--- src/Dialogs/Settings/Panels/VarioConfigPanel.cpp
+++ src/Dialogs/Settings/Panels/VarioConfigPanel.cpp
@@ -2,79 +2,83 @@
 // Copyright The XCSoar Project
 
 #include "VarioConfigPanel.hpp"
-#include "Profile/Keys.hpp"
-#include "Language/Language.hpp"
+#include "ConfigListPanel.hpp"
 #include "Interface.hpp"
-#include "Widget/RowFormWidget.hpp"
-#include "UIGlobals.hpp"
+#include "Language/Language.hpp"
+#include "Profile/Keys.hpp"
+#include "Profile/Profile.hpp"
 
-enum ControlIndex {
-  AppGaugeVarioSpeedToFly,
-  AppGaugeVarioAvgText,
-  AppGaugeVarioMc,
-  AppGaugeVarioBugs,
-  AppGaugeVarioBallast,
-  AppGaugeVarioGross,
-  AppAveNeedle,
-  AppAveThermalNeedle,
-};
+/**
+ * What the vario gauge shows besides the needle: the values around
+ * it, and the needles which stand for averages.  All of it is for
+ * the expert user level.
+ */
+class VarioConfigPanel final : public ConfigListPanel {
+  VarioSettings settings;
 
-
-class VarioConfigPanel final : public RowFormWidget {
-public:
-  VarioConfigPanel()
-    :RowFormWidget(UIGlobals::GetDialogLook()) {}
+protected:
+  /* virtual methods from class ConfigListPanel */
+  void LoadSettings() noexcept override;
+  void Fill() noexcept override;
 
 public:
-  void Prepare(ContainerWindow &parent, const PixelRect &rc) noexcept override;
+  /* virtual methods from class Widget */
   bool Save(bool &changed) noexcept override;
 };
 
 void
-VarioConfigPanel::Prepare(ContainerWindow &parent,
-                          const PixelRect &rc) noexcept
+VarioConfigPanel::LoadSettings() noexcept
 {
-  const VarioSettings &settings = CommonInterface::GetUISettings().vario;
+  settings = CommonInterface::GetUISettings().vario;
+}
 
-  RowFormWidget::Prepare(parent, rc);
+void
+VarioConfigPanel::Fill() noexcept
+{
+  if (!IsExpert())
+    return;
 
-  AddBoolean(_("Speed arrows"),
-             _("Whether to show speed command arrows on the vario gauge. In cruise mode, "
-                 "arrows pointing up command slow down; arrows pointing down command speed up."),
-             settings.show_speed_to_fly);
-  SetExpertRow(AppGaugeVarioSpeedToFly);
+  AddGroup();
 
-  AddBoolean(_("Show average"),
-             _("Whether to show the average climb rate. In cruise mode, this switches to showing the "
-                 "average netto airmass rate."),
-             settings.show_average);
-  SetExpertRow(AppGaugeVarioAvgText);
+  AddToggleItem(_("Speed arrows"),
+                _("Whether to show speed command arrows on the vario gauge. In cruise mode, "
+                  "arrows pointing up command slow down; arrows pointing down command speed up."),
+                settings.show_speed_to_fly);
 
-  AddBoolean(_("Show MacReady"), _("Whether to show the MacCready setting."), settings.show_mc);
-  SetExpertRow(AppGaugeVarioMc);
+  AddToggleItem(_("Show average"),
+                _("Whether to show the average climb rate. In cruise mode, this switches to showing the "
+                  "average netto airmass rate."),
+                settings.show_average);
 
-  AddBoolean(_("Show bugs"), _("Whether to show the bugs percentage."), settings.show_bugs);
-  SetExpertRow(AppGaugeVarioBugs);
+  AddToggleItem(_("Show MacReady"),
+                _("Whether to show the MacCready setting."),
+                settings.show_mc);
 
-  AddBoolean(_("Show ballast"), _("Whether to show the ballast percentage."), settings.show_ballast);
-  SetExpertRow(AppGaugeVarioBallast);
+  AddToggleItem(_("Show bugs"),
+                _("Whether to show the bugs percentage."),
+                settings.show_bugs);
 
-  AddBoolean(_("Show gross"), _("Whether to show the gross climb rate."), settings.show_gross);
-  SetExpertRow(AppGaugeVarioGross);
+  AddToggleItem(_("Show ballast"),
+                _("Whether to show the ballast percentage."),
+                settings.show_ballast);
 
-  AddBoolean(_("Averager needle"),
-             _("If true, the vario gauge will display a hollow averager needle. During cruise, this "
-                 "needle displays the average netto value. During circling, this needle displays the "
-                 "average gross value."),
-             settings.show_average_needle);
-  SetExpertRow(AppAveNeedle);
+  AddToggleItem(_("Show gross"),
+                _("Whether to show the gross climb rate."),
+                settings.show_gross);
 
-  AddBoolean(_("Thermal Averager needle"),
-             _("If true, the vario gauge will display a thermal averager needle instead of the current climb-rate needle. During cruise, this "
-               "needle displays the last thermal average netto value. During circling, this needle displays the "
-               "average net value."),
-             settings.show_thermal_average_needle);
-  SetExpertRow(AppAveThermalNeedle);
+  AddGroup();
+
+  AddToggleItem(_("Averager needle"),
+                _("If true, the vario gauge will display a hollow averager needle. During cruise, this "
+                  "needle displays the average netto value. During circling, this needle displays the "
+                  "average gross value."),
+                settings.show_average_needle);
+
+  AddToggleItem(_("Thermal Averager needle"),
+                _("If true, the vario gauge will display a thermal averager needle instead of the current climb-rate needle. During cruise, this "
+                  "needle displays the last thermal average netto value. During circling, this needle displays the "
+                  "average net value."),
+                settings.show_thermal_average_needle);
 }
 
 bool
@@ -82,23 +86,34 @@ VarioConfigPanel::Save(bool &_changed) noexcept
 {
   bool changed = false;
 
-  VarioSettings &settings = CommonInterface::SetUISettings().vario;
+  VarioSettings &live = CommonInterface::SetUISettings().vario;
 
-  changed |= SaveValue(AppGaugeVarioSpeedToFly, ProfileKeys::AppGaugeVarioSpeedToFly, settings.show_speed_to_fly);
+  changed |= Profile::Update(ProfileKeys::AppGaugeVarioSpeedToFly,
+                             live.show_speed_to_fly,
+                             settings.show_speed_to_fly);
 
-  changed |= SaveValue(AppGaugeVarioAvgText, ProfileKeys::AppGaugeVarioAvgText, settings.show_average);
+  changed |= Profile::Update(ProfileKeys::AppGaugeVarioAvgText,
+                             live.show_average, settings.show_average);
 
-  changed |= SaveValue(AppGaugeVarioMc, ProfileKeys::AppGaugeVarioMc, settings.show_mc);
+  changed |= Profile::Update(ProfileKeys::AppGaugeVarioMc,
+                             live.show_mc, settings.show_mc);
 
-  changed |= SaveValue(AppGaugeVarioBugs, ProfileKeys::AppGaugeVarioBugs, settings.show_bugs);
+  changed |= Profile::Update(ProfileKeys::AppGaugeVarioBugs,
+                             live.show_bugs, settings.show_bugs);
 
-  changed |= SaveValue(AppGaugeVarioBallast, ProfileKeys::AppGaugeVarioBallast, settings.show_ballast);
+  changed |= Profile::Update(ProfileKeys::AppGaugeVarioBallast,
+                             live.show_ballast, settings.show_ballast);
 
-  changed |= SaveValue(AppGaugeVarioGross, ProfileKeys::AppGaugeVarioGross, settings.show_gross);
+  changed |= Profile::Update(ProfileKeys::AppGaugeVarioGross,
+                             live.show_gross, settings.show_gross);
 
-  changed |= SaveValue(AppAveNeedle, ProfileKeys::AppAveNeedle, settings.show_average_needle);
+  changed |= Profile::Update(ProfileKeys::AppAveNeedle,
+                             live.show_average_needle,
+                             settings.show_average_needle);
 
-  changed |= SaveValue(AppAveThermalNeedle, ProfileKeys::AppAveThermalNeedle, settings.show_thermal_average_needle);
+  changed |= Profile::Update(ProfileKeys::AppAveThermalNeedle,
+                             live.show_thermal_average_needle,
+                             settings.show_thermal_average_needle);
 
   _changed |= changed;
 
