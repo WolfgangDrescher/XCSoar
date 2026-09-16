15a30145bbc68e1305271293f8706676f7f4c5eb
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2026-09-16 21:02:53 +0000

    Dialogs/Settings/Panels/InfoBoxesConfigPanel: Show the sets as a grouped list which opens each

--- src/Dialogs/Settings/Panels/InfoBoxesConfigPanel.cpp
+++ src/Dialogs/Settings/Panels/InfoBoxesConfigPanel.cpp
@@ -2,39 +2,46 @@
 // Copyright The XCSoar Project
 
 #include "InfoBoxesConfigPanel.hpp"
+#include "ConfigListPanel.hpp"
 #include "../dlgConfigInfoboxes.hpp"
 #include "Profile/Profile.hpp"
 #include "Profile/Current.hpp"
 #include "Profile/InfoBoxConfig.hpp"
-#include "Form/Button.hpp"
 #include "Interface.hpp"
-#include "Widget/RowFormWidget.hpp"
 #include "Language/Language.hpp"
 #include "UIGlobals.hpp"
 #include "Look/Look.hpp"
 
-class InfoBoxesConfigPanel final
-  : public RowFormWidget {
-public:
-  InfoBoxesConfigPanel()
-    :RowFormWidget(UIGlobals::GetDialogLook()) {}
+/**
+ * The sets of InfoBoxes: one item per set, which opens the view
+ * where its InfoBoxes are chosen.
+ */
+class InfoBoxesConfigPanel final : public ConfigListPanel {
+  bool use_final_glide;
+
+private:
+  /** Open the view which edits the InfoBoxes of the set @p i. */
+  void EditPanel(unsigned i) noexcept;
+
+protected:
+  /* virtual methods from class ConfigListPanel */
+  void LoadSettings() noexcept override;
+  void Fill() noexcept override;
 
 public:
-  void Prepare(ContainerWindow &parent, const PixelRect &rc) noexcept override;
+  /* virtual methods from class Widget */
   bool Save(bool &changed) noexcept override;
-
-  void OnAction(int id) noexcept;
 };
 
 void
-InfoBoxesConfigPanel::OnAction(int id) noexcept
+InfoBoxesConfigPanel::EditPanel(unsigned i) noexcept
 {
   InfoBoxSettings &settings = CommonInterface::SetUISettings().info_boxes;
-
-  unsigned i = (unsigned)id;
   InfoBoxSettings::Panel &data = settings.panels[i];
 
-  bool changed =
+  /* the set is stored right away, unlike the other settings, which
+     wait for the dialog to be closed */
+  const bool changed =
     dlgConfigInfoboxesShowModal(UIGlobals::GetMainWindow(),
                                 UIGlobals::GetDialogLook(),
                                 UIGlobals::GetLook().info_box,
@@ -43,38 +50,49 @@ InfoBoxesConfigPanel::OnAction(int id) noexcept
   if (changed) {
     Profile::Save(Profile::map, data, i);
     Profile::Save();
-    ((Button &)GetRow(i)).SetCaption(gettext(data.name));
+    Refresh();
   }
 }
 
 void
-InfoBoxesConfigPanel::Prepare(ContainerWindow &parent,
-                              const PixelRect &rc) noexcept
+InfoBoxesConfigPanel::LoadSettings() noexcept
 {
-  const InfoBoxSettings &settings = CommonInterface::GetUISettings().info_boxes;
+  use_final_glide =
+    CommonInterface::GetUISettings().info_boxes.use_final_glide;
+}
 
-  RowFormWidget::Prepare(parent, rc);
+void
+InfoBoxesConfigPanel::Fill() noexcept
+{
+  const InfoBoxSettings &settings =
+    CommonInterface::GetUISettings().info_boxes;
 
-  for (unsigned i = 0; i < InfoBoxSettings::MAX_PANELS; i++) {
-    const InfoBoxSettings::Panel &data = settings.panels[i];
+  AddGroup(_("InfoBox Sets"));
 
-    AddButton(gettext(data.name), [this, i](){ OnAction(i); });
-    if (i>2)
-      SetExpertRow(i);
-  }
+  /* the three sets of the automatic pages, and the others for the
+     expert */
+  const unsigned n = IsExpert()
+    ? InfoBoxSettings::MAX_PANELS
+    : InfoBoxSettings::PREASSIGNED_PANELS;
 
-  AddBoolean(_("Use final glide mode"),
-             _("Controls whether the \"final glide\" InfoBox mode should be used on \"auto\" pages."),
-             settings.use_final_glide);
+  for (unsigned i = 0; i < n; ++i)
+    AddItem(gettext(settings.panels[i].name), [this, i](){ EditPanel(i); },
+            {.chevron = true});
+
+  AddGroup();
+
+  AddToggleItem(_("Use final glide mode"),
+                _("Controls whether the \"final glide\" InfoBox mode should be used on \"auto\" pages."),
+                use_final_glide);
 }
 
 bool
-InfoBoxesConfigPanel::Save([[maybe_unused]] bool &_changed) noexcept
+InfoBoxesConfigPanel::Save(bool &_changed) noexcept
 {
   InfoBoxSettings &settings = CommonInterface::SetUISettings().info_boxes;
-  SaveValue(InfoBoxSettings::MAX_PANELS, ProfileKeys::UseFinalGlideDisplayMode,
-            settings.use_final_glide);
 
+  _changed |= Profile::Update(ProfileKeys::UseFinalGlideDisplayMode,
+                              settings.use_final_glide, use_final_glide);
   return true;
 }
 
