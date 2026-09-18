695d97e8bdc1f81a12c8e7f89a1261b5ba1f5a44
Wolfgang Drescher <drescher.wolfgang@gmail.com>
2026-09-18 08:04:47 +0000

    ui/canvas/Util: Add corner selection and outlines to RoundRect()

--- src/ui/canvas/Util.cpp
+++ src/ui/canvas/Util.cpp
@@ -8,6 +8,8 @@
 
 static constexpr unsigned CIRCLE_SEGS = 64;
 
+static_assert(ROUND_RECT_MAX_POINTS == (CIRCLE_SEGS + 2) * 4);
+
 [[gnu::const]]
 static PixelPoint
 CirclePoint(int radius, unsigned angle) noexcept
@@ -149,31 +151,119 @@ KeyHole(Canvas &canvas, PixelPoint center, unsigned radius,
   return true;
 }
 
-void
-RoundRect(Canvas &canvas, PixelRect r, unsigned radius) noexcept
+/**
+ * Append the points of one corner of a rounded rectangle: the arc if
+ * the corner is rounded, or just the corner point.
+ */
+static void
+RoundRectCornerPoints(BulkPixelPoint *pt, unsigned &npoly,
+                      const PixelRect &r, unsigned radius,
+                      RoundRectCorner corner, bool rounded) noexcept
+{
+  switch (corner) {
+  case ROUND_TOP_LEFT:
+    if (rounded)
+      segment_poly(pt, r.GetTopLeft().At(radius, radius), radius,
+                   INT_ANGLE_RANGE * 3 / 4,
+                   INT_ANGLE_RANGE - 1,
+                   npoly);
+    else
+      pt[npoly++] = r.GetTopLeft();
+    break;
+
+  case ROUND_TOP_RIGHT:
+    if (rounded)
+      segment_poly(pt, r.GetTopRight().At(-(int)radius, radius), radius,
+                   0, INT_ANGLE_RANGE / 4 - 1,
+                   npoly);
+    else
+      pt[npoly++] = r.GetTopRight();
+    break;
+
+  case ROUND_BOTTOM_RIGHT:
+    if (rounded)
+      segment_poly(pt, r.GetBottomRight().At(-(int)radius, -(int)radius),
+                   radius,
+                   INT_ANGLE_RANGE / 4,
+                   INT_ANGLE_RANGE / 2 - 1,
+                   npoly);
+    else
+      pt[npoly++] = r.GetBottomRight();
+    break;
+
+  case ROUND_BOTTOM_LEFT:
+    if (rounded)
+      segment_poly(pt, r.GetBottomLeft().At(radius, -(int)radius), radius,
+                   INT_ANGLE_RANGE / 2,
+                   INT_ANGLE_RANGE * 3 / 4 - 1,
+                   npoly);
+    else
+      pt[npoly++] = r.GetBottomLeft();
+    break;
+
+  case ROUND_ALL_CORNERS:
+    break;
+  }
+}
+
+/**
+ * The corners in clockwise order, starting at the top left.
+ */
+static constexpr RoundRectCorner round_rect_corners[] = {
+  ROUND_TOP_LEFT, ROUND_TOP_RIGHT, ROUND_BOTTOM_RIGHT, ROUND_BOTTOM_LEFT,
+};
+
+unsigned
+RoundRectPoints(BulkPixelPoint *pt, PixelRect r, unsigned radius,
+                unsigned corners) noexcept
 {
   unsigned npoly = 0;
-  BulkPixelPoint pt[(CIRCLE_SEGS+2)*4];
 
-  segment_poly(pt, r.GetTopLeft().At(radius, radius), radius,
-               INT_ANGLE_RANGE * 3 / 4,
-               INT_ANGLE_RANGE - 1,
-               npoly);
-  segment_poly(pt, r.GetTopRight().At(-(int)radius, radius), radius,
-               0, INT_ANGLE_RANGE / 4 - 1,
-               npoly);
-  segment_poly(pt, r.GetBottomRight().At(-(int)radius, -(int)radius), radius,
-               INT_ANGLE_RANGE / 4,
-               INT_ANGLE_RANGE / 2 - 1,
-               npoly);
-  segment_poly(pt, r.GetBottomLeft().At(radius, -(int)radius), radius,
-               INT_ANGLE_RANGE / 2,
-               INT_ANGLE_RANGE * 3 / 4 - 1,
-               npoly);
+  for (const RoundRectCorner corner : round_rect_corners)
+    RoundRectCornerPoints(pt, npoly, r, radius, corner, corners & corner);
 
-  assert(npoly <= ARRAY_SIZE(pt));
+  assert(npoly <= ROUND_RECT_MAX_POINTS);
+  return npoly;
+}
+
+void
+RoundRect(Canvas &canvas, PixelRect r, unsigned radius,
+          unsigned corners) noexcept
+{
+  BulkPixelPoint pt[ROUND_RECT_MAX_POINTS];
+  const unsigned npoly = RoundRectPoints(pt, r, radius, corners);
+
   if (npoly)
     canvas.DrawTriangleFan(pt, npoly);
+}
+
+void
+RoundRectOutline(Canvas &canvas, PixelRect r, unsigned radius,
+                 unsigned corners, unsigned edges) noexcept
+{
+  /* the edges in clockwise order, starting at the top; each runs from
+     round_rect_corners[i] to round_rect_corners[i + 1] */
+  static constexpr RoundRectEdge round_rect_edges[] = {
+    ROUND_RECT_TOP, ROUND_RECT_RIGHT, ROUND_RECT_BOTTOM, ROUND_RECT_LEFT,
+  };
+
+  BulkPixelPoint pt[(CIRCLE_SEGS+2)*2];
+
+  for (unsigned i = 0; i < 4; ++i) {
+    if ((edges & round_rect_edges[i]) == 0)
+      continue;
+
+    const RoundRectCorner start = round_rect_corners[i];
+    const RoundRectCorner end = round_rect_corners[(i + 1) % 4];
+
+    unsigned npoly = 0;
+    RoundRectCornerPoints(pt, npoly, r, radius, start, corners & start);
+    RoundRectCornerPoints(pt, npoly, r, radius, end, corners & end);
+
+    assert(npoly <= ARRAY_SIZE(pt));
+    if (npoly >= 2)
+      canvas.DrawPolyline(pt, npoly);
+  }
 }
 
 bool
--- src/ui/canvas/Util.hpp
+++ src/ui/canvas/Util.hpp
@@ -12,6 +12,7 @@ class Canvas;
 class Angle;
 struct PixelPoint;
 struct PixelRect;
+struct BulkPixelPoint;
 
 bool
 Segment(Canvas &canvas, PixelPoint center, unsigned radius,
@@ -25,8 +26,64 @@ bool
 KeyHole(Canvas &canvas, PixelPoint center, unsigned radius,
         Angle start, Angle end, unsigned inner_radius) noexcept;
 
+/**
+ * Corner flags for RoundRect().
+ */
+enum RoundRectCorner : unsigned {
+  ROUND_TOP_LEFT = 0x1,
+  ROUND_TOP_RIGHT = 0x2,
+  ROUND_BOTTOM_RIGHT = 0x4,
+  ROUND_BOTTOM_LEFT = 0x8,
+  ROUND_ALL_CORNERS = 0xf,
+};
+
+/**
+ * The maximum number of points RoundRectPoints() generates.
+ */
+static constexpr unsigned ROUND_RECT_MAX_POINTS = (64 + 2) * 4;
+
+/**
+ * Generate the polygon (a triangle fan) of a rectangle whose selected
+ * corners are rounded, as filled by RoundRect().
+ *
+ * @param pt room for at least #ROUND_RECT_MAX_POINTS points
+ * @return the number of points written
+ */
+unsigned
+RoundRectPoints(BulkPixelPoint *pt, PixelRect r, unsigned radius,
+                unsigned corners) noexcept;
+
+/**
+ * Fill a rectangle whose selected corners are rounded.
+ *
+ * @param corners a bit mask of #RoundRectCorner; the other corners
+ * stay square
+ */
 void
-RoundRect(Canvas &canvas, PixelRect r, unsigned radius) noexcept;
+RoundRect(Canvas &canvas, PixelRect r, unsigned radius,
+          unsigned corners=ROUND_ALL_CORNERS) noexcept;
+
+/**
+ * Edge flags for RoundRectOutline().
+ */
+enum RoundRectEdge : unsigned {
+  ROUND_RECT_TOP = 0x1,
+  ROUND_RECT_RIGHT = 0x2,
+  ROUND_RECT_BOTTOM = 0x4,
+  ROUND_RECT_LEFT = 0x8,
+};
+
+/**
+ * Draw the outline of the shape RoundRect() fills, but only along
+ * the selected edges, with the currently selected pen.  A rounded
+ * corner belongs to both edges it joins.
+ *
+ * @param corners a bit mask of #RoundRectCorner
+ * @param edges a bit mask of #RoundRectEdge
+ */
+void
+RoundRectOutline(Canvas &canvas, PixelRect r, unsigned radius,
+                 unsigned corners, unsigned edges) noexcept;
 
 bool
 Arc(Canvas &canvas, PixelPoint center, unsigned radius,
